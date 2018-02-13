8be8eb1afb25d7428bf23ee1abc1b43f7a9912e6
anjalisg <anjalisgopinathan@gmail.com>
2018-02-13 11:07:04 -0800

    Unit 6 not yet completed

---
+++ Lab09f.java
@@ -0,0 +1,25 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class Lab09f
+{
+	public static void main( String args[] )
+	{
+		//add test cases
+		Scanner read = new Scanner(System.in);
+		
+		LetterRemover extract = new LetterRemover();
+		//extract.LetterRemover(read.next());
+		extract.removeLetters();
+		
+		
+											
+	}
+}
