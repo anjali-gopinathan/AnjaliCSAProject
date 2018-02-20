445742bcf5dc7e7d5bc4b7ec3e26bbe33eebd242
anjalisg <anjalisgopinathan@gmail.com>
2018-02-20 11:33:15 -0800

    lab 7 triples not yet done
    triples has some errors

--- Lab11j.java
+++ Lab11j.java
@@ -11,5 +11,7 @@ public class Lab11j
    public static void main(String args[])
    {
 	   Triples test = new Triples();
+	   test.setNum(110);
+	   System.out.println(test);
    }
 }
--- Triples.java
+++ Triples.java
@@ -28,21 +28,39 @@ public class Triples
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
-		//for()
 
-
-		return 1;
+		for(int i=0; i<c; i++){
+			if( (   (a%i == 0) && (b%i ==0)   ) && (c%i ==0) ){
+				max=i;
+			}
+		}
+		return max;
 	}
 
 	public String toString()
 	{
 		String output="";
+			
+		for(int a=0; a<number; a++){
+			for(int b=0; b<number; b++){
+				for(int c=0; c<number; c++){
+					if(  (c*c) == ( (a*a)+ (b*b) )  ){
+						output +="\npythag equation true\n";
+						if(((a%2 == 1 && b%2 == 0)||(a%2==0 && b%2==1)  )&&(c%2==1)){//if a is odd
+							output+="2nd condition true with all those odd/even things\n";
+							if(greatestCommonFactor(a,b,c) <=1){
+								output+="3rd condition true, gcf <=1\n";
+								output+=a + "\t"+b +"\t"+ c + "\n";
+							}
+						}
+					}
+				}
+			}
+		}
 
 
 
 
-
-
-		return output+"\n";
+		return ""+output+"\n";
 	}
 }
