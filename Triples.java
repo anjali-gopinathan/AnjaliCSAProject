116eb44b5ff68783f2fec2400312f8368ec81df5
anjalisg <anjalisgopinathan@gmail.com>
2018-02-21 09:49:51 -0800

    Triples and 11j complete

--- Triples.java
+++ Triples.java
@@ -27,9 +27,9 @@ public class Triples
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-		int max = 0;
+		int max = 1;
 
-		for(int i=0; i<c; i++){
+		for(int i=1; i<c; i++){
 			if( (   (a%i == 0) && (b%i ==0)   ) && (c%i ==0) ){
 				max=i;
 			}
@@ -41,24 +41,24 @@ public class Triples
 	{
 		String output="";
 			
-		for(int a=0; a<number; a++){
-			for(int b=0; b<number; b++){
+		for(int a=1; a<number; a++){
+			for(int b=1; b<number; b++){
 				for(int c=0; c<number; c++){
 					if(  (c*c) == ( (a*a)+ (b*b) )  ){
-						output +="\npythag equation true\n";
-						if(((a%2 == 1 && b%2 == 0)||(a%2==0 && b%2==1)  )&&(c%2==1)){//if a is odd
-							output+="2nd condition true with all those odd/even things\n";
-							if(greatestCommonFactor(a,b,c) <=1){
-								output+="3rd condition true, gcf <=1\n";
-								output+=a + "\t"+b +"\t"+ c + "\n";
+						//output +="\npythag equation true\n";
+						if(  (  (a%2 == 1 && b%2 == 0)||(a%2==0 && b%2==1)  )&&(c%2==1)){//if a is odd
+							//output+="2nd condition true with all those odd/even things\n";
+							if(c>b && (b>a && c>a)){
+								if(greatestCommonFactor(a,b,c) <=1){
+									//output+="3rd condition true, gcf <=1\n";
+									output+=a + "\t"+b +"\t"+ c + "\n";
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-
-
 
 
 		return ""+output+"\n";
