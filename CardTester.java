6c5cbe6a5e1beb0ca443c347087836d200621513
anjalisg <anjalisgopinathan@gmail.com>
2018-02-22 11:12:48 -0800

    Unit 9 not yet complete
    card and cardTester complete

---
+++ CardTester.java
@@ -0,0 +1,16 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card c1 = new Card("10", "hearts", 10);
+		Card c2 = new Card("A",  "spades", 11);		//??is an ace 11 points???
+		Card c3 = new Card("4",  "clubs",  4)
+	}
+}
