d79a65494d5b457d3cb5ad827171591edbeb9eaa
anjalisg <anjalisgopinathan@gmail.com>
2018-03-21 15:32:56 -0700

    barely started unit 14

---
+++ FancyWord.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class FancyWord
+{
+	private char[][] mat;
+
+	public FancyWord()
+	{
+
+	}
+
+   public FancyWord(String s)
+	{
+
+
+
+
+	}
+
+	public String toString()
+	{
+		String output="";
+		for(int r=0; r<mat.length; r++){
+//			for(int c=0; c<mat[r]; c++){
+//				
+//			}
+		}
+
+
+
+
+		return output+"\n\n";
+	}
+}
