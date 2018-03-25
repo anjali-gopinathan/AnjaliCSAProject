ac978e90e8d24249443fb7dd657f9c40f54f7b8e
anjalisg <anjalisgopinathan@gmail.com>
2018-03-25 12:51:56 -0700

    started on fancyWord and ticTacToe

--- Lab24d.java
+++ Lab24d.java
@@ -13,9 +13,15 @@ public class Lab24d
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner (new File("Lab24d.dat"));
-
-
+		Scanner file = new Scanner (new File("C:\\Users\\anjgo\\Documents\\APCSA\\Unit14_2016\\Unit14-Assignments\\Lab24d.dat"));
+		int size = file.nextInt();
+		file.nextLine();
+		for(int i=0; i<size; i++) {
+			String xo = file.nextLine();
+			TicTacToe test = new TicTacToe(xo);
+			test.getWinner();
+			System.out.println(test);
+		}
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -13,13 +13,19 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
-
+		mat = new char[3][3];
 	}
 
 	public TicTacToe(String game)
 	{
-
+//		Scanner read = new Scanner(game);
+		int i=0;
+		for(int r=0; r<3; r++) {
+			for(int c=0; c<3; c++) {
+				mat[r][c] = game.charAt(i);
+				i++;
+			}
+		}
 
 
 
@@ -27,30 +33,60 @@ public class TicTacToe
 
 	public String getWinner()
 	{
+		boolean xWins=false, oWins=false;
+		String direction="";
+		//checks for horizontal winner
+		for(int r=0; r<3; r++) {
+			if(mat[r][0] == mat[r][1] && mat[r][1]==mat[r][2]) {
+				direction = "horizontally";
+				if(mat[r][0]=='X') {
+					xWins = true;
+					return "X wins "+direction;
+				}
+				if(mat[r][0]=='O') {
+					oWins = true;
+					return "O wins "+direction;
+				}
+			}
+		}
+		//checks for vertical winner
+		for(int c=0; c<3; c++) {
+			if(mat[0][c] == mat[1][c] && mat[1][c] == mat[2][c]) {
+				direction = "vertically";
+				if(mat[0][c]=='X') xWins = true;
+				if(mat[0][c]=='O') oWins = true;
+			}
+		}
+		//checks for diagonal winner (aahhh hard coding is bad)
+		if(  (mat[0][0]==mat[1][1] && mat[1][1]==mat[2][2])
+		   ||(mat[0][2]==mat[1][1] && mat[1][1]==mat[2][0]) ){
+			if(mat[0][0]=='X') xWins = true;
+			if(mat[0][0]=='O') oWins = true;
+			direction = "diagonally";
+		}
 
+//		for(int r=0; r<3; r++) {
+//			for(int c=0; c<3; c++) {
+//				if(mat[r][c] == mat)
+//			}
+//		}
 
-
-
-
-
-
-
-
-
-
-
+		if(xWins) return "X wins" + direction + "!";
+		if(oWins) return "O wins" + direction + "!";
+		if(!xWins && !oWins)  return "cat's game - no winner!";
 		return "";
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
+		for(int r=0; r<3; r++) {
+			for(int c=0; c<3; c++) {
+				output+= mat[r][c]+" ";
+			}
+			output+="\n";
+		}
+		output+=getWinner();
 		return output+"\n\n";
 	}
 }
