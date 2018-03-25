de339940a2c90e7e49e344a78884df63743d40da
anjalisg <anjalisgopinathan@gmail.com>
2018-03-25 11:26:54 -0700

    FancyWord and 24b almost done, but has errors

--- FancyWord.java
+++ FancyWord.java
@@ -13,28 +13,27 @@ public class FancyWord
 
 	public FancyWord()
 	{
-
+		mat = new char[0][0];
 	}
 
    public FancyWord(String s)
 	{
-
-
-
-
+	   for(int i=0; i<s.length(); i++) {
+		   mat[0][i] = s.charAt(i);
+		   mat[i][i] = s.charAt(i);
+		   mat[s.length()][i] = s.charAt(i);
+	   }
 	}
 
 	public String toString()
 	{
 		String output="";
-		for(int r=0; r<mat.length; r++){
-//			for(int c=0; c<mat[r]; c++){
-//				
-//			}
+		for(int r=0; r<mat.length; r++) {
+			for(int c=0; c<mat[0].length; c++) {
+				output+=mat[r][c]+" ";
+			}
+			output+="\n";
 		}
-
-
-
 
 		return output+"\n\n";
 	}
