eaeccc05b724f9d075371f05e1de8386d33a13aa
anjalisg <anjalisgopinathan@gmail.com>
2018-03-30 10:30:22 -0700

    MergeSort works yay

--- Lab22d.java
+++ Lab22d.java
@@ -9,11 +9,12 @@ public class Lab22d
 	public static void main( String args[] )
 	{
 		MergeSort.mergeSort(new Comparable[]{9,5,3,2});
-
-
+		System.out.println();
+		
 		MergeSort.mergeSort(new Comparable[]{19,52,3,2,7,21});
-
+		System.out.println();
 
 		MergeSort.mergeSort(new Comparable[]{68,66,11,2,42,31});
+		System.out.println();
 	}
 }
--- MergeSort.java
+++ MergeSort.java
@@ -20,39 +20,50 @@ public class MergeSort
 	private static void mergeSort( Comparable[] list, int front, int back)  //O( Log N )
 	{
 		int mid = (front+back)/2;
-		if(list.length ==1) {
-			return;
-		}
+		if(mid == front) return;
+//		if(list.length ==1) return;
+		mergeSort(list, front, mid);
+		mergeSort(list, mid, back);
+		merge(list, front, back);
 		//mergeSort()
-
-
-
-
 
 	}
 
 	private static void merge(Comparable[] list, int front, int back)  //O(N)
 	{
+		
+		Comparable[] arr = new Comparable[back - front];
+		int i=front, j=(front+back)/2, k=0, mid = j;
+		
+		while(i<mid && j<back) {
+			
+			if(list[i].compareTo(list[j])<0) {
+				arr[k] = list[i];
+				i++;
+			}
+			else {
+				arr[k] = list[j];
+				j++;
+			}
+//			System.out.println("pass "+k+ Arrays.toString(list));
+			k++;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		}
+		while(i<mid) {
+			arr[k] = list[i];
+			k++;
+			i++;
+		}
+		while(j<back) {
+			arr[k] = list[j];
+			k++;
+			j++;
+		}
+		for(int indx = 0; indx<(back-front); ++indx) {
+			list[front+indx] = arr[indx];
+		}
+		System.out.println("pass "+ passCount + " "+Arrays.toString(list));
+		passCount++;
 
 	}
 }
