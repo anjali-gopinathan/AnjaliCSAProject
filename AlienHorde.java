5a2ef8f20c88d8d4365747a2d839d266c8304b54
anjalisg <anjalisgopinathan@gmail.com>
2018-04-27 11:12:43 -0700

    U17 done

--- AlienHorde.java
+++ AlienHorde.java
@@ -1,63 +1,73 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date - 
-//Class -
-//Lab  -
-
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import javax.imageio.ImageIO;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AlienHorde
-{
+public class AlienHorde {
 	private List<Alien> aliens;
+	private int size;
+	private int tick;
+	private int origSize;
 
-	public AlienHorde(int size)
-	{
-		aliens = new ArrayList<Alien>(size);
+	public AlienHorde(int size) {
+		aliens = new ArrayList<Alien>();
+		this.size = size;
+		origSize = size;
 	}
 
-	public void add(Alien al)
-	{
+	public void add(Alien al) {
 		aliens.add(al);
 	}
 
-	public void drawEmAll( Graphics window )
-	{
-		for(Alien a : aliens) {
-			a.draw(window);
+	public void moveEmAll() {
+		tick++;
+		for (Alien al : aliens) {
+			if (tick <= 120)
+				al.move("RIGHT");
+			else if (tick <= 320)
+				al.move("DOWN");
+			else if (tick <= 440)
+				al.move("LEFT");
+			else if (tick <= 640)
+				al.move("UP");
+			else if (tick <= 1000)
+				tick = 0;
 		}
 	}
 
-	public void moveEmAll()
-	{
-		for(Alien a : aliens){
-			while(a.getX()>40){
-				a.move("LEFT");
-			}
-			while(a.getX()<600){
-				a.move("RIGHT");
-			}
-		}
+	public void drawEmAll(Graphics window) {
+		for (Alien al : aliens)
+			al.draw(window);
 	}
 
-	public void removeDeadOnes(List<Ammo> shots)
-	{
+	public void removeDeadOnes(List<Ammo> shots) {
+		for (int i = 0; i < shots.size(); i++)
+			for (int j = 0; j < aliens.size(); j++)
+				try {
+					if (shots.get(i).isCollide(aliens.get(j))) {
+						shots.remove(i);
+						aliens.remove(j);
+						size--;
+					}
+				} catch (Exception e) {
+				}
+	}
+
+	public void checkShipDeath(Ship ship) {
+		for (int i = 0; i < aliens.size(); i++)
+			if (ship.isCollide(aliens.get(i))) {
+				System.out.println("You lose. \nNumber of aliens killed = " + (origSize - aliens.size()));
+				System.exit(0);
+			}
+	}
+	public Alien getAlien(int indx){
+		return aliens.get(indx);
+	}
 	
+	public int getSize() {
+		return size;
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		return "";
-	}
-
-	public List<Alien> getAliens() {
-		// TODO Auto-generated method stub
-		return aliens;
 	}
 }
--- OuterSpace.java
+++ OuterSpace.java
@@ -1,233 +1,175 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class - 
-//Lab  -
-
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Canvas;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
-import static java.lang.Character.*;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
-public class OuterSpace extends Canvas implements KeyListener, Runnable
-{
+
+/**
+ * 
+ * CROP alien.JPG and ship.jpg AS SMALL AS YOU CAN WITHOUT DAMAGING PICTURE FOR OPTIMAL COLLISIONS
+ *
+ */
+
+
+
+@SuppressWarnings("serial")
+public class OuterSpace extends Canvas implements KeyListener, Runnable {
+
 	private Ship ship;
-	private Alien alienOne;
-	private Alien alienTwo;
-	private Ammo ammo;
-	private AlienHorde horde;
 	private Bullets shots;
 
 	private int tick;
 
+	private AlienHorde horde;
 	private boolean[] keys;
 	private BufferedImage back;
 
-	public OuterSpace()
-	{
-
+	public OuterSpace() {
 		setBackground(Color.black);
 
 		keys = new boolean[5];
 
-		horde = new AlienHorde(0);
+		ship = new Ship(400, 500, 35, 35, 2);
+		
+		int hordeWidth = 7;
+		int hordeHeight = 4;
+		int hordeSize = hordeWidth * hordeHeight;
+		
+		horde = new AlienHorde(hordeSize);
 		shots = new Bullets();
-		ship = new Ship(100,100,50,50,6);
-		alienOne = new Alien(80,80,6);
-		alienTwo = new Alien(120,100,6);
-		ammo = new Ammo();
+		
+		for(int x = 8; x < StarFighter.WIDTH - 100; x += (StarFighter.WIDTH) / 8)
+			for(int y = 22; y < StarFighter.HEIGHT / 1.5; y += (StarFighter.HEIGHT / 1.5) / 4)
+				horde.add(new Alien(x + 20, y, 25, 25, 1));
+
 		this.addKeyListener(this);
 		new Thread(this).start();
 
-		
-		for (int x = 8; x < StarFighter.WIDTH - 100; x += (StarFighter.WIDTH) / 8){
-			for (int y = 22; y < StarFighter.HEIGHT / 1.5; y += (StarFighter.HEIGHT / 1.5) / 4){
-				horde.add(new Alien(x + 20, y, 25, 25, 1));
-			}
-		}
-		
 		setVisible(true);
 	}
 
-   public void update(Graphics window)
-   {
-	   paint(window);
-   }
+	public void update(Graphics window) {
+		paint(window);
+	}
 
-	public void paint( Graphics window )
-	{
-		//set up the double buffering to make the game animation nice and smooth
-		Graphics2D twoDGraph = (Graphics2D)window;
-
-		//take a snap shop of the current screen and same it as an image
-		//that is the exact same width and height as the current screen
-		if(back==null)
-		   back = (BufferedImage)(createImage(getWidth(),getHeight()));
-
-		//create a graphics reference to the back ground image
-		//we will draw all changes on the background image
+	public void paint(Graphics window) {
+		Graphics2D twoDGraph = (Graphics2D) window;
+		if (back == null)
+			back = (BufferedImage) (createImage(getWidth(), getHeight()));
 		Graphics graphToBack = back.createGraphics();
 
 		graphToBack.setColor(Color.BLUE);
-		graphToBack.drawString("StarFighter ", 25, 50 );
+		graphToBack.drawString("StarFighter ", 25, 50);
 		graphToBack.setColor(Color.BLACK);
-		graphToBack.fillRect(0,0,800,600);
-		
+		graphToBack.fillRect(0, 0, 800, 600);
+
 		tick++;
-		
+
+		// processInputs
 		if(ship.getX()>0){
-			if(keys[0] == true)
-			{
-				ship.move("LEFT");
+		if(keys[0] == true)
+		{
+			ship.move("LEFT");
+		}
+	}
+	if(ship.getX()<740){
+		if(keys[1] == true)
+		{
+			ship.move("RIGHT");
+		}
+	}
+	if(ship.getY()>0){
+		if(keys[2] == true)
+		{
+			ship.move("UP");
+		}
+	}
+	if(ship.getY()<510){
+		if(keys[3] == true)
+		{
+			ship.move("DOWN");
+		}
+	}
+		if (keys[4] == true) {
+			if (tick >= 70) {
+				shots.add(new Ammo(ship.getX() + ship.getWidth() / 2 - 2, ship.getY(), 5));
+				tick = 0;
 			}
 		}
-		if(ship.getX()<740){
-			if(keys[1] == true)
-			{
-				ship.move("RIGHT");
-			}
-		}
-		if(ship.getY()>0){
-			if(keys[2] == true)
-			{
-				ship.move("UP");
-			}
-		}
-		if(ship.getY()<500){
-			if(keys[3] == true)
-			{
-				ship.move("DOWN");
-			}
-		}
-		if(keys[4] == true){
-			Ammo am = new Ammo(ship.getX() + ship.getWidth() / 2 - 2, ship.getY(), 5);
-//			ammo.move("UP");
-			
-			shots.add(am);
 
-			keys[4] = false;
-
-		}
-		
-//		for (int i = 0; i < shots.getAmmo().size(); i++)
-//			for (int j = 0; j < horde.getAliens().size(); j++)
-//				if (shots.getAmmo().get(i).isColliding(horde.getAliens().get(j))) {
-//					shots.getAmmo().remove(i--);
-//					horde.getAliens().remove(j--);
-//				}
-		
-		
-
-		
-		//add code to move Ship, Alien, etc.
-		ship.draw(graphToBack);
-		ammo.draw(graphToBack);
-
-		//add in collision detection to see if Bullets hit the Aliens and if Bullets hit the Ship
-		shots.drawEmAll(graphToBack);
-		shots.moveEmAll();
-
-		shots.cleanEmUp();
-		
-		horde.removeDeadOnes(shots.getList());
+		// update
 		horde.moveEmAll();
-		horde.drawEmAll(graphToBack);
-		
+		shots.moveEmAll();
+		horde.removeDeadOnes(shots.getList());
+		shots.cleanEmUp();
+		horde.checkShipDeath(ship);
+
+		if (horde.getSize() == 0) {
+			System.out.println("You win!");
+			System.exit(0);
+		}
+
+		// render
+		graphToBack.setColor(Color.WHITE);
+		graphToBack.drawString(""+horde.getSize(), 740, 530);
 		ship.draw(graphToBack);
-		
-		
+		shots.drawEmAll(graphToBack);
+		horde.drawEmAll(graphToBack);
 
-		
-		
-		while(alienOne.getX()>20){			
-			alienOne.move("LEFT");
-		}
-		while(alienOne.getX()<650){
-			alienOne.move("RIGHT");
-		}
-		while(alienTwo.getX()>20){			
-			alienTwo.move("LEFT");
-		}
-		while(alienTwo.getX()<650){
-			alienTwo.move("RIGHT");
-		}	
-
+		// draw back image
 		twoDGraph.drawImage(back, null, 0, 0);
-
-		back = null;
 	}
 
-
-	public void keyPressed(KeyEvent e)
-	{
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			keys[0] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			keys[1] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_UP)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
 			keys[2] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			keys[3] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_A) {
 			keys[4] = true;
 		}
-		repaint();
 	}
 
-	public void keyReleased(KeyEvent e)
-	{
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			keys[0] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			keys[1] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_UP)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
 			keys[2] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			keys[3] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_A) {
 			keys[4] = false;
 		}
-		repaint();
 	}
 
-	public void keyTyped(KeyEvent e){}
+	public void keyTyped(KeyEvent e) {
+	}
 
-   public void run()
-   {
-	   try
-	   {
-		   while(true)
-		   {
-			   Thread.currentThread().sleep(5);
-			   repaint();
-		   }
-	   }
-	   catch(Exception e){ }
-  	}
+	public void run() {
+		try {
+			while (true) {
+				Thread.currentThread();
+				Thread.sleep(5);
+				repaint();
+			}
+		} catch (Exception e) {
+		}
+	}
 }
-
